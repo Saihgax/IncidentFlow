83bac58dd932cc765a95d157173d91195d4e6226
Saihgax <pranav@Pranavs-MacBook-Pro.local>
2025-04-04 19:58:15 +0530

    fixed main.go to run server only when required

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "incidentflow/internal/server"
+import (
+	"incidentflow/internal/server"
+	"incidentflow/cmd"
+	"os"
+)
+
 
 func main() {
-	server.StartServer()
+	// Check if the first argument is "generate" (CLI mode)
+	if len(os.Args) > 1 && os.Args[1] == "generate" {
+		cmd.Execute() // Run CLI command
+	} else {
+		server.StartServer() // Otherwise, start API server
+	}
 }
